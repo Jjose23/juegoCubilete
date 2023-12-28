17d01468c51bcc63f6c39d1fcf242817ca49a252
JuanRodriguez <jjoserodriguez182@gmail.com>
2023-12-28 17:16:53 -0300

    migracion al DOM, Storage y JSON

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 
-
 const dados = ["1", "2", "3", "4", "5", "6"];
-const cantidadDeLanzamientos = 3; // cantidad de lanzamientos
-
+let cantidadDeLanzamientos = 0; // cantidad de lanzamientos
 // Función para obtener valores aleatorios permitiendo repeticiones dentro del mismo array
 function obtenerValoresAleat(dados, cantidad) {
     const resultados = [];
@@ -26,29 +24,53 @@ function lanzarCubilete(usuario) {
 }
 
 // usuarios
-let usuario1 = { nombre: '', resultados: [], rondasGanadas:0 };
-let usuario2 = { nombre: 'Usuario2', resultados: [], rondasGanadas:0 };
+let usuario1 = { nombre: '', resultados: [], rondasGanadas:0, historial:[] };
+let usuario2 = { nombre: 'CPU', resultados: [], rondasGanadas:0, historial:[] };
 usuario1.nombre = prompt("Por favor, ingrese su nombre:");
 
-// Realiza lanzamientos  para cada usuario
-for (let lanzamiento = 1; lanzamiento <= cantidadDeLanzamientos; lanzamiento++) {
+let userName = document.querySelector('.user');
+let btnLanzar = document.querySelector('.btn-lanzar');
+let marcadorUser = document.querySelector('.marcadorUser')
+let marcadorCpu = document.querySelector('.marcadorCpu')
+let puntuacionUser = document.querySelector('.puntuacion-user')
+let puntuacionCpu = document.querySelector('.puntuacion-cpu')
+let ganadorRonda = document.querySelector('.ganador-ronda')
+let ganador = document.querySelector('.ganador')
+
+userName.textContent =usuario1.nombre;
+btnLanzar.addEventListener('click', function() {
+
+  if (cantidadDeLanzamientos < 3) {
     lanzarCubilete(usuario1);
     lanzarCubilete(usuario2);
-
-  // Condiciones para determinar el ganador de la ronda
+    cantidadDeLanzamientos++;
+    //Muestra la puntuacion de cada usuario en el navegador
+    puntuacionUser.textContent = `${usuario1.nombre} lanza los dados y obtiene...${usuario1.resultados}`;
+    puntuacionCpu.textContent = `${usuario2.nombre} lanza los dados y obtiene...${usuario2.resultados}`;
+    
     if (usuario1.resultados === usuario2.resultados) {
-    console.log('Empate');
-    } else if (usuario1.resultados.reduce((a, b) => a + b, 0) > usuario2.resultados.reduce((a, b) => a + b, 0)) {
-    console.log(`${usuario1.nombre} gana la ronda`);
-    usuario1.rondasGanadas++;
+        console.log('Empate');
     } else {
-    console.log('Usuario2 gana la ronda');
-    usuario2.rondasGanadas++
+        const ganador = usuario1.resultados.reduce((a, b) => a + b, 0) > usuario2.resultados.reduce((a, b) => a + b, 0)
+            ? (usuario1.rondasGanadas++, usuario1.nombre)
+            : (usuario2.rondasGanadas++, 'CPU');
+
+        ganadorRonda.textContent = `El usuario ${ganador} gana la ronda`;
+        marcadorUser.textContent = usuario1.rondasGanadas;
+        marcadorCpu.textContent = usuario2.rondasGanadas;
     }
+    //añade todos los resultados de cada usuario en historial y lo almacena en localStorage en formato JSON
+    usuario1.historial.push(usuario1.resultados)
+    usuario2.historial.push(usuario2.resultados)
+    localStorage.setItem('Usuario', JSON.stringify(usuario1))
+    localStorage.setItem('CPU', JSON.stringify(usuario2))
 }
-console.log('------------------------------------------------');
-if(usuario1.rondasGanadas > usuario2.rondasGanadas){
-    console.log(`------------- ${usuario1.nombre} gana el juego-------------`);
-    console.log('------------------------------------------------');
-}else{console.log('-------------Usuario2 gana el juego-------------');
-console.log('------------------------------------------------');}
+// Condionales para obtener el ganador por mayoria de rondas ganadas
+if (cantidadDeLanzamientos === 3 && usuario1.rondasGanadas > usuario2.rondasGanadas) {
+  ganador.textContent = `El usuario ${usuario1.nombre} por mayoria de puntos ha ganado el juego`;
+
+} else if (cantidadDeLanzamientos === 3) {
+  ganador.textContent = `El usuario ${usuario2.nombre} por mayoria de puntos ha ganado el juego`;
+}
+
+});
